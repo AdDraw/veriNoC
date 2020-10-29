d3e9802aad77df6bcda88d394c8826a161b2e36a
AdDraw <drawc.adam.priv@gmail.com>
2020-10-29 23:05:35 +0100

    XY_Switch: first xy_switch contender(without FIFOs on the inputs) - added a yosys script for it - initialized a simulation cocotb project (still doesnt work, still needs adaptation)

--- srcs/switch/simple_mesh_xy/xy_switch.v
+++ srcs/switch/simple_mesh_xy/xy_switch.v
@@ -57,12 +57,12 @@ module xy_switch
     output busy_o,
 
     // SWITCH INPUTS
-    input pckt_vld_sw_i [NEIGHBOURS_N-1 : 0],
-    input [`PACKET_W - 1 : 0] packet_sw_i [NEIGHBOURS_N - 1 : 0],
+    input [NEIGHBOURS_N-1 : 0] pckt_vld_sw_i,
+    input [`PACKET_W * NEIGHBOURS_N - 1 : 0] pckt_sw_i,
     // SWITCH OUTPUTS
-    output pckt_rd_sw_o [NEIGHBOURS_N-1 : 0],
-    output pckt_vld_sw_o [NEIGHBOURS_N-1 : 0],
-    output [`PACKET_W - 1 : 0] pckt_sw_o [NEIGHBOURS_N - 1 : 0],
+    output [NEIGHBOURS_N-1 : 0] pckt_rd_sw_o,
+    output [NEIGHBOURS_N-1 : 0] pckt_vld_sw_o,
+    output [`PACKET_W * NEIGHBOURS_N - 1 : 0] pckt_sw_o,
 
     // RESOURCE INPUT
     input pckt_vld_r_i,
@@ -74,72 +74,107 @@ module xy_switch
     output [`PACKET_W - 1 : 0] pckt_r_o
     );
 
+    // Wires
+    wire busy_w = (|{pckt_vld_sw_i, pckt_vld_r_i} == 1'b1) ? 1'b1 : 1'b0;
+    reg [`PACKET_W - 1 : 0] tmp_pckt_w;
+    wire [`PACKET_ADDR_X_W - 1 : 0] x_addr = tmp_pckt_w[`PACKET_W-1 : `PACKET_W - `PACKET_ADDR_X_W];
+    wire [`PACKET_ADDR_Y_W - 1 : 0] y_addr = tmp_pckt_w[`PACKET_W - `PACKET_ADDR_X_W - 1 : `PACKET_DATA_W];
 
-    reg [`PACKET_W - 1 : 0] tmp_pckt;
+    wire [`PACKET_W -1 : 0 ] pckt_sw_i_w [NEIGHBOURS_N - 1 : 0];
+    genvar i;
+    generate
+      for (i=0; i < NEIGHBOURS_N; i = i + 1) begin
+        assign pckt_sw_i_w[i] = pckt_sw_i[(i+1)*`PACKET_W -1 : i*`PACKET_W ];
+      end
+    endgenerate
+
+
+    // Registers
     reg [`PACKET_W - 1 : 0] pckt_r_v;
     reg pckt_vld_r_v;
     reg [`PACKET_W - 1 : 0] pckt_sw_v [NEIGHBOURS_N - 1 : 0] ;
     reg [NEIGHBOURS_N-1 : 0] pckt_vld_sw_v;
 
+    // Vars
+    integer iter;
 
-    // DATA 2 Algo Handler // Input Data Arbiter
+    // Input Data Arbiter
     /*
       Chooses which packet should be routed
 
-    */
+      v0.5 - simple and with/ justice
+       - LEFT has most priority
+       - after that TOP
+       - after that RIGHT
+       - after that BOTTOm
+       - after that RESOURCE
 
+      v1.0 - RoundRobin + hopcounts (not yet done)
+
+    */
+    always @(*) begin
+      if (|{pckt_vld_sw_i, pckt_vld_r_i}) begin
+        casez ({pckt_vld_sw_i[3:0], pckt_vld_r_i})
+          5'b1????: tmp_pckt_w = pckt_sw_i_w[0]; // LEFT always has priority
+          5'b01???: tmp_pckt_w = pckt_sw_i_w[1];
+          5'b001??: tmp_pckt_w = pckt_sw_i_w[2];
+          5'b0001?: tmp_pckt_w = pckt_sw_i_w[3];
+          5'b00001: tmp_pckt_w = pckt_r_i;
+          default : tmp_pckt_w = 'h0;
+        endcase
+      end
+      else begin
+        tmp_pckt_w = 'h0;
+      end
+    end
 
     // XY algorithm
     /*
       first X movement (at X = X_addr move in Y axis)
       next Y movement (stop at Y=Y_addr)
 
-
-
-      nxt_state -> where the data should be placed
-      cur_state -> where the data is being placed
-
-
     */
     always @(posedge clk_i or negedge rst_ni) begin
 
       if (!rst_ni) begin
+        pckt_r_v <= 'b0;
+        pckt_vld_r_v <= 1'b0;
 
+        for (iter=0; iter < NEIGHBOURS_N; iter = iter + 1) begin
+          pckt_sw_v[iter] <= 'b0;
+          pckt_vld_sw_v[iter] <= 'b0 ;
+        end
       end
       else begin
-        var [`PACKET_ADDR_X_W - 1 : 0] x_addr = tmp_pckt[`PACKET_W-1 : `PACKET_W - `PACKET_ADDR_X_W];
-        var [`PACKET_ADDR_Y_W - 1 : 0] y_addr = tmp_pckt[`PACKET_W - `PACKET_ADDR_X_W - 1 : `PACKET_DATA_W];
-
         if (x_addr == X_CORD ) begin
           // X movement Finished, start ELEVATION or DEELEVATION
           // NEIGHBOUR 0 - left
           // NEIGHBOUR 1 - top
           // NEIGHBOUR 2 - right
           // NEIGHBOUR 3 - bottom
-
           if (y_addr == Y_CORD) begin
           // Y movement Finished, route to Resource
-            pckt_r_v <= tmp_pckt;
+            pckt_r_v <= tmp_pckt_w;
             pckt_vld_r_v <= 1'b1;
           end
           else if (y_addr > Y_CORD) begin
             //Y movement not finished
-            pckt_sw_v[`BOT] <= tmp_pckt;
+            pckt_sw_v[`BOT] <= tmp_pckt_w;
             pckt_vld_sw_v[`BOT] <= 1'b1;
           end
           else begin
-            pckt_sw_v[`TOP] <= tmp_pckt;
+            pckt_sw_v[`TOP] <= tmp_pckt_w;
             pckt_vld_sw_v[`TOP] <= 1'b1;
           end
         end
         else begin
           if (x_addr > X_CORD) begin
             //Y movement not finished
-            pckt_sw_v[`RIGHT] <= tmp_pckt;
+            pckt_sw_v[`RIGHT] <= tmp_pckt_w;
             pckt_vld_sw_v[`RIGHT] <= 1'b1;
           end
           else begin
-            pckt_sw_v[`LEFT] <= tmp_pckt;
+            pckt_sw_v[`LEFT] <= tmp_pckt_w;
             pckt_vld_sw_v[`LEFT] <= 1'b1;
           end
         end
@@ -147,6 +182,13 @@ module xy_switch
 
     end
 
+    generate
+      for (i=0; i < NEIGHBOURS_N; i = i + 1) begin
+        assign pckt_sw_o[(i+1)*`PACKET_W -1 : i*`PACKET_W ] = pckt_sw_v[i];
+      end
+    endgenerate
 
-    assign pckt_sw_o = pckt_sw_v;
+    assign pckt_vld_sw_o = pckt_vld_sw_v;
+    assign busy_o = busy_w;
+
 endmodule
