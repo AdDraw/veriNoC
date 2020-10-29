3eb3c84432f4f4619b7df1cec53e8b7002bc7d0a
AdDraw <drawc.adam.priv@gmail.com>
2020-10-29 23:05:35 +0100

    XY_Switch: first xy_switch contender(without FIFOs on the inputs) - added a yosys script for it - initialized a simulation cocotb project (still doesnt work, still needs adaptation)

---
+++ sim/xy_switch/mon_o.py
@@ -0,0 +1,41 @@
+import cocotb
+from cocotb.monitors import BusMonitor
+from cocotb.triggers import RisingEdge, ReadOnly
+
+from logging import INFO, DEBUG
+
+# TODO: Make a working version
+
+class SWOMon(BusMonitor):
+    _signals = ["busy_o", "pckt_sw_o", "pckt_r_o", "pckt_vld_r_o", "pckt_rd_sw_o"]
+
+    def __init__(self, entity, name, clock, log_lvl=INFO, callback=None):
+        self.entity = entity
+        self.name = name
+        self.clock = clock
+        BusMonitor.__init__(self, entity, name, clock, callback=callback)
+        if log_lvl == DEBUG:
+            self.log.setLevel(DEBUG)
+        else:
+            self.log.setLevel(INFO)
+
+    @cocotb.coroutine
+    async def _monitor_recv(self):
+        # Monitor Init
+        clkedg = RisingEdge(self.clock)
+        ro = ReadOnly()
+
+        while True:
+            # Capture the output data
+            await clkedg
+            await ro
+            bus_values = self.bus.capture()
+            data_o = bus_values["data_o"].value
+            empty_o = bus_values["empty_o"].value
+            full_o = bus_values["full_o"].value
+            underflow_o = bus_values["underflow_o"].value
+            overflow_o = bus_values["overflow_o"].value
+
+            cycle_results = [data_o, empty_o, full_o, underflow_o, overflow_o]
+            self.log.debug(f'T_o: {cycle_results}')
+            self._recv(cycle_results)
