3efc3ca1ab8dcb398842156683520bf9b4d1a1ef
AdDraw <drawc.adam.priv@gmail.com>
2021-02-22 23:29:41 +0100

    Single SWITCH TB: updates - added a gtkwave template - chose to use `iverilog` as the simulator since it supports tapping into modules instantiated with generate loops - Updated the mon_i & mon_o - updated the driver - fixed the makefile - enabled Pycharm debug attaching - added a necessary tb.v file to dump VCD from icarus - Drivers and monitors now need a config as an input (instead of taking params from the os.environ) - started working on the mon_i (still thinking about the best way to validate) - slight updates to the circ_fifo_sim & sample_test tbs

--- sim/xy_switch/driver.py
+++ sim/xy_switch/driver.py
@@ -8,28 +8,36 @@ from logging import INFO, DEBUG
 
 import os
 
-# TODO: Test if it works
-
 
 class SWPacketDriver(BusDriver):
     _signals = ["pckt_sw_i", "wr_en_sw_i", "nxt_fifo_full_i", "nxt_fifo_overflow_i"]
 
-    def __init__(self, entity, name, clock, log_lvl=INFO):
+    _default_config = {
+        "packet_x_addr_w": 2,
+        "packet_y_addr_w": 2,
+        "packet_data_w": 8,
+        "packet_w": 12,
+        "neighbours_n": 5,
+        "fifo_depth_w": 2,
+        "x_cord": 0,
+        "y_cord": 0
+    }
+
+    def __init__(self, entity, name, clock, config=None, log_lvl=INFO):
         self.entity = entity
         self.name = name
         self.clock = clock
+
+        if config is None:
+            self.config = self._default_config
+        else:
+            self.config = config
+
         BusDriver.__init__(self, entity, name, clock)
         if log_lvl == DEBUG:
             self.log.setLevel(DEBUG)
         else:
             self.log.setLevel(INFO)
-
-        # Load Parameter Values
-        self.packet_x_addr_width = int(os.environ["PACKET_X_ADDR_W"])
-        self.packet_y_addr_width = int(os.environ["PACKET_Y_ADDR_W"])
-        self.packet_data_width = int(os.environ["PACKET_DATA_W"])
-        self.packet_width = int(os.environ["PACKET_W"])
-        self.neighbours_n = int(os.environ["PORT_N"])
 
         # Reset Input signals
         self.bus.pckt_sw_i.setimmediatevalue(0)
@@ -37,28 +45,31 @@ class SWPacketDriver(BusDriver):
         self.bus.nxt_fifo_full_i.setimmediatevalue(0)
         self.bus.nxt_fifo_overflow_i.setimmediatevalue(0)
 
+        self.packet_id = 1
+
     async def n_rand_data_packets(self, n):
         for i in range(n):
             await RisingEdge(self.clock)
-            self.bus.pckt_sw_i <= getrandbits(self.packet_width * self.neighbours_n)
+            self.bus.pckt_sw_i <= getrandbits(self.config["packet_w"] * self.config["neighbours_n"])
 
-    async def write_to_single_input(self, input_id, packet_data, x_dest, y_dest):
-        if 0 > input_id > 2**self.neighbours_n:
+    async def write_to_single_input(self, input_id, x_dest, y_dest):
+        if 0 > input_id > 2**self.config["neighbours_n"]:
             raise TestError("input id is not correct")
 
-        packet = packet_data
-        packet |= (y_dest << self.packet_data_width)
-        packet |= (x_dest << (self.packet_data_width + self.packet_y_addr_width))
-        packets = BinaryValue(0, self.neighbours_n*self.packet_width, bigEndian=False)
-        packets |= (packet << input_id * self.packet_width)
+        packet = self.packet_id
+        packet |= (y_dest << self.config["packet_data_w"])
+        packet |= (x_dest << (self.config["packet_data_w"] + self.config["packet_y_addr_w"]))
+        packets = BinaryValue(0, self.config["neighbours_n"]*self.config["packet_w"], bigEndian=False)
+        packets |= (packet << input_id * self.config["packet_w"])
 
         self.bus.pckt_sw_i <= packets
-        self.bus.wr_en_sw_i <= BinaryValue(1 << input_id, self.neighbours_n, bigEndian=False)
+        self.bus.wr_en_sw_i <= BinaryValue(1 << input_id, self.config["neighbours_n"], bigEndian=False)
         await RisingEdge(self.clock)
 
-        self.bus.pckt_sw_i <= BinaryValue(0, self.neighbours_n*self.packet_width, bigEndian=False)
-        self.bus.wr_en_sw_i <= BinaryValue(0, self.neighbours_n, bigEndian=False)
+        self.bus.pckt_sw_i <= BinaryValue(0, self.config["neighbours_n"]*self.config["packet_w"], bigEndian=False)
+        self.bus.wr_en_sw_i <= BinaryValue(0, self.config["neighbours_n"], bigEndian=False)
 
+        self.packet_id += 1
 
     async def clear_sw_input(self, sync: bool = True):
         if sync is True:
--- srcs/switch/simple_mesh_xy/crossbar.v
+++ srcs/switch/simple_mesh_xy/crossbar.v
@@ -32,34 +32,36 @@ module n_to_n_crossbar
     reg [DATA_WIDTH - 1 : 0] mux_out_data_v [PORT_N - 1 : 0];
     // reg [DATA_WIDTH - 1 : 0] mux_in_data_chosen_w;
 
-    genvar i;
+    genvar gi;
     generate
       wire [DATA_WIDTH -1 : 0]  mux_in [PORT_N - 1 : 0];
       // Input data unroll
-      for( i = 0; i < PORT_N; i = i + 1)
+      for( gi = 0; gi < PORT_N; gi = gi+ 1)
       begin
-        assign mux_in[i] = data_i[DATA_WIDTH*(i+1) - 1 : DATA_WIDTH*i];
+        assign mux_in[gi] = data_i[DATA_WIDTH*(gi+1) - 1 : DATA_WIDTH*gi];
       end
       // INPUT MUX
       wire [DATA_WIDTH - 1 : 0] mux_in_data_chosen_w = mux_in[mux_in_sel_i];
 
     endgenerate
 
+    integer i;
+
     // OUTPUT MUX, with registers
     always @(*)
     begin
-      mux_out_data_v[mux_out_sel_i] = mux_in_data_chosen_w;
       for (i = 0; i < PORT_N; i = i + 1)
       begin
-        mux_out_data_v[i] = mux_out_data_v[i];
+        mux_out_data_v[i] = 0;
       end
+      mux_out_data_v[mux_out_sel_i] = mux_in_data_chosen_w;
     end
 
     // Wrapping data_o
     generate
-      for( i = 0; i < PORT_N; i = i + 1)
+      for( gi = 0; gi < PORT_N; gi = gi + 1)
       begin
-        assign data_o[DATA_WIDTH*(i+1) - 1 : DATA_WIDTH*i] = mux_out_data_v[i];
+        assign data_o[DATA_WIDTH*(gi+1) - 1 : DATA_WIDTH*gi] = mux_out_data_v[gi];
       end
     endgenerate
     assign pckt_in_chosen_o = mux_in_data_chosen_w;
