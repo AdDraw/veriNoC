480808567e35dd5efc615989e50f624888498dc2
AdDraw <drawc.adam.priv@gmail.com>
2021-02-28 19:24:38 +0100

    FIFO TB: Fixes after changes were done to FIFO RTL module - fixed overflow and underflow - cycle_results now as dicts instead of lists( more visibility) - fixed the Makefile for icarus verilog simulator - removed `almost_full` `almost_empty` functionality

--- sim/circ_fifo_sim/make.py
+++ sim/circ_fifo_sim/make.py
@@ -6,17 +6,13 @@ PROF = 0
 CLEAN = 0
 MUTE = False
 os.environ["HDL_LANG"] = "verilog"
-os.environ["SIM"] = "verilator"
+os.environ["SIM"] = "icarus"
 
 DATA_WIDTH = 8
 FIFO_DEPTH_WIDTH = 8
-ALMOST_EMPTY_LEVEL = 2
-ALMOST_FULL_LEVEL = 8
 
 os.environ["DATA_WIDTH"] = f"{DATA_WIDTH}"
 os.environ["FIFO_DEPTH_WIDTH"] = f"{FIFO_DEPTH_WIDTH}"
-os.environ["ALMOST_FULL_LEVEL"] = f"{FIFO_DEPTH_WIDTH}'d{ALMOST_FULL_LEVEL}"
-os.environ["ALMOST_EMPTY_LEVEL"] = f"{FIFO_DEPTH_WIDTH}'d{ALMOST_EMPTY_LEVEL}"
 
 # MAKEFILE call
 if PROF == 1:
