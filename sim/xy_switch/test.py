Simple TB for the XYSwitch: Passes simulation - fixed problems in RTL:   - router was routing packets up instead of down   - Fifo underflow and overflow was not being cleared when full/empty = 0   - pckt_sw_o now equal to 0 when |vld_input == 0 - TB improvements:   - Switch Driver now with single & multi input data driving   - ScoreBoard is no longer used( it's very useful for comb or pipelined modules but in this case it's hard to get a refernce module for every cycle)   - CONFIG object that passes HDL parameters to python objects instead of directly calling os.environ[] everytime   - Functionality is now being tested by checking if the packet id seen on the output port equals the id it was assigned ( ID is present in packet_data section)   - Output Monitor now extracts ID & dst instead of simply passing outputs to the Scoreboarding class(whcich no longer exists)   - Test.py now has 3 test scenarios and using TestFactory we now test the design many times over

sim/xy_switch/test.py
@@ -1,17 +1,15 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.result import TestSuccess, TestError
+from cocotb.result import TestSuccess, TestError, TestFailure
 from cocotb.triggers import ClockCycles, RisingEdge
-from cocotb.scoreboard import Scoreboard
-
 from driver import SWPacketDriver
 from mon_i import SWIMon
 from mon_o import SWOMon
-
-from fifo_imon import FifoIMon
-
 from logging import INFO, DEBUG
+from cocotb.log import SimLog
+from cocotb.regression import TestFactory
 import os
+from random import randint, getrandbits
 
 CLOCK_PERIOD_NS = 10
 
@@ -35,7 +33,9 @@ class SWTB:
     def __init__(self, dut, log_lvl=INFO):
         self.dut = dut
         self.dut._discover_all()
-        # cocotb.log.info(f"SubHandles: {self.dut._sub_handles}")
+
+        self.log = SimLog("swtb")
+        self.log.setLevel(log_lvl)
 
         self.config = {
             "packet_x_addr_w": int(os.environ["PACKET_X_ADDR_W"]),
@@ -48,30 +48,93 @@ class SWTB:
             "y_cord": int(os.environ["Y_CORD"])
         }
 
+        self.run_stats = []
+
         self.expected_out = []
+        self.seen_out = []
         self.sw_drv = SWPacketDriver(dut, "", dut.clk_i, self.config, log_lvl)
-        self.sw_i_mon = SWIMon(dut, "", dut.clk_i, self.config, log_lvl, callback=self.mon_callback)
-        # self.sw_o_mon = SWOMon(dut, "", dut.clk_i, log_lvl)
-        # self.sb = Scoreboard(dut, fail_immediately=False)
-        # self.sb.add_interface(self.sw_o_mon, self.expected_out)
+        self.sw_i_mon = SWIMon(dut, "", dut.clk_i, self.config, log_lvl, callback=self.mon_i_callback)
+        self.sw_o_mon = SWOMon(dut, "", dut.clk_i, self.config, log_lvl, callback=self.mon_o_callback)
 
     def setup_dut(self, cycle_n):
-        cocotb.fork(self.reset_sw())
+        cocotb.fork(self.reset_hdl())
         cocotb.fork(Clock(self.dut.clk_i, CLOCK_PERIOD_NS).start(cycles=cycle_n))
 
-    async def reset_sw(self):
+    def reset_swtb(self):
+        self.expected_out = []
+        self.seen_out = []
+        self.sw_drv.reset()
+        self.sw_i_mon.reset()
+        self.sw_o_mon.reset()
+
+    async def reset_hdl(self):
         await RisingEdge(self.dut.clk_i)
         self.dut.rst_ni <= 0
 
         await ClockCycles(self.dut.clk_i, 5)
         self.dut.rst_ni <= 1
 
-    def mon_callback(self, t):
+    def mon_i_callback(self, t):
         self.expected_out.append(t)
+
+    def mon_o_callback(self, t):
+        self.seen_out.append(t)
+
+    def compare(self):
+        self.log.info("\n------- Simulation Statistics ---------\n")
+
+        self.log.debug(f"Expected Packets N {self.expected_out.__len__()}")
+        self.expected_out = sorted(self.expected_out, key=lambda k: k['id'])
+        for rid, record in enumerate(self.expected_out):
+            self.log.debug(f"{rid}. expected {record}")
+
+        full_input = self.expected_out.copy()
+        for record in self.sw_i_mon.loss_packets:
+            full_input.append(record)
+        full_input = sorted(full_input, key=lambda k: k['id'])
+        self.log.debug(f"Lenght of Full {full_input.__len__()}")
+
+        # for rid, record in enumerate(full_input):
+        #     self.log.info(f"{rid}. full {record}")
+
+        sorted_seen = sorted(self.seen_out, key=lambda k: k['id'])
+        for rid, rd2 in enumerate(sorted_seen):
+            if rd2["id"] == self.expected_out[rid]["id"]:
+                self.log.debug(f"comapre {rd2}, {self.expected_out[rid]}")
+            else:
+                self.log.debug(f"comapre {rd2}, {self.expected_out[rid]}")
+
+        dropped_pckt_n = self.sw_drv.sent_packets - self.sw_o_mon.packets_received
+        if dropped_pckt_n == self.sw_i_mon.loss_packets.__len__():
+            self.log.info(f"Correctly dropped {self.sw_i_mon.loss_packets.__len__()} packets"
+                          f" out of {self.sw_drv.sent_packets} sent packets")
+        else:
+            self.log.error(f"Dropped {dropped_pckt_n} packets "
+                           f"instead of expected {self.sw_i_mon.loss_packets.__len__()} packets!"
+                           f" from {self.sw_drv.sent_packets} sent packets")
+
+        if len(self.expected_out) == len(sorted_seen):
+            self.log.info(f"Correct number of packets was routed {len(sorted_seen)}/{len(self.expected_out)}")
+
+            fail_cnt = 0
+            att_cnt = 0
+            for r_e, r_s in zip(self.expected_out, sorted_seen):
+                if r_e != r_s:
+                    self.log.warning(f"{fail_cnt}: {r_e} != {r_s}")
+                    fail_cnt += 1
+                att_cnt += 1
+
+            if fail_cnt > 0:
+                raise TestFailure(f"Incorrectly routed packets {fail_cnt}/{att_cnt}")
+            else:
+                self.log.info("All packets that were not dropped were properly routed!")
+                raise TestSuccess(f"All packets that were not dropped were properly routed!")
+        else:
+            raise TestFailure(f"Incorrect number of packets was routed {len(sorted_seen)}/{len(self.expected_out)}")
 
 
 @cocotb.test()
-async def overflow_underflow_test(dut, log_lvl=INFO, cycles=1000):
+async def test_255(dut, log_lvl=INFO, cycles=100000):
     cocotb.log.info("----------------------------------------------------------------------------- Simulation Started!")
 
     if int(os.environ['DEBUG_ATTACH']) > 0:
@@ -82,40 +145,133 @@ async def overflow_underflow_test(dut, log_lvl=INFO, cycles=1000):
     swtb.setup_dut(cycle_n=cycles)
 
     # Clear FIFO inputs
+    swtb.reset_swtb()
     await swtb.sw_drv.clear_sw_input()
+    await swtb.reset_hdl()
 
     await ClockCycles(dut.clk_i, 10)
 
-    # Reset The FIFO
-    # await swtb.sw_drv.reset_sw()
+    cocotb.fork(swtb.sw_drv.gen_rand_full(255))
 
-    await ClockCycles(dut.clk_i, 2)
+    await send255(swtb)
 
-    await RisingEdge(dut.clk_i)
-    dut.nxt_fifo_full_i <= pow(2, 5) - 1
-
-    await send5way(swtb)
-    await send5way(swtb)
-    await send5way(swtb)
-    await send5way(swtb)
-    await send5way(swtb)
-    await send5way(swtb)
-    await send5way(swtb)
-    await send5way(swtb)
-    await send5way(swtb)
-
-    await ClockCycles(dut.clk_i, 10)
-
+    await swtb.sw_drv.clear_sw_input(True)
     dut.nxt_fifo_full_i <= 0
+
+    await ClockCycles(dut.clk_i, 10)
 
     await ClockCycles(dut.clk_i, 100)
 
-    raise TestSuccess("Sim finished sucessfully")
+    swtb.compare()
+
+
+@cocotb.test()
+async def test_rand_single_input(dut, log_lvl=INFO, transaction_w=2, with_nxt_fifo_rand=True, cycles=100000):
+    cocotb.log.info("----------------------------------------------------------------------------- Simulation Started!")
+
+    if int(os.environ['DEBUG_ATTACH']) > 0:
+        import pydevd_pycharm
+        pydevd_pycharm.settrace('localhost', port=9090, stdoutToServer=True, stderrToServer=True)
+
+    swtb = SWTB(dut.xy_switch_inst, log_lvl)
+    swtb.setup_dut(cycle_n=cycles)
+
+    # Clear FIFO inputs
+    swtb.reset_swtb()
+
+    await swtb.sw_drv.clear_sw_input()
+    await swtb.reset_hdl()
+
+    await ClockCycles(dut.clk_i, 10)
+
+    if transaction_w <= swtb.config["packet_data_w"]:
+        transaction_n = pow(2, transaction_w) - 1
+    else:
+        transaction_n = pow(2, swtb.config["packet_data_w"]) - 1
+
+    if with_nxt_fifo_rand is True:
+        cocotb.fork(swtb.sw_drv.gen_rand_full(transaction_n))
+
+    for i in range(transaction_n):
+        src = randint(0, 4)
+        x_dest = getrandbits(swtb.config["packet_x_addr_w"])
+        y_dest = getrandbits(swtb.config["packet_y_addr_w"])
+        await swtb.sw_drv.write_to_single_input(input_id=src, x_dest=x_dest, y_dest=y_dest)
+
+    await swtb.sw_drv.clear_sw_input(True)
+    dut.nxt_fifo_full_i <= 0
+
+    await ClockCycles(dut.clk_i, 10)
+
+    await ClockCycles(dut.clk_i, 100)
+
+    swtb.compare()
+
+
+@cocotb.test()
+async def test_rand_multi_input(dut, log_lvl=INFO, transaction_w=2, with_nxt_fifo_rand=True, cycles=100000):
+    cocotb.log.info("----------------------------------------------------------------------------- Simulation Started!")
+
+    if int(os.environ['DEBUG_ATTACH']) > 0:
+        import pydevd_pycharm
+        pydevd_pycharm.settrace('localhost', port=9090, stdoutToServer=True, stderrToServer=True)
+
+    swtb = SWTB(dut.xy_switch_inst, log_lvl)
+    swtb.setup_dut(cycle_n=cycles)
+
+    # Clear FIFO inputs
+    swtb.reset_swtb()
+
+    await swtb.sw_drv.clear_sw_input()
+    await swtb.reset_hdl()
+
+    await ClockCycles(dut.clk_i, 10)
+
+    if transaction_w <= swtb.config["packet_data_w"]:
+        transaction_n = pow(2, transaction_w) - 1
+    else:
+        transaction_n = pow(2, swtb.config["packet_data_w"]) - 1
+
+    if with_nxt_fifo_rand is True:
+        cocotb.fork(swtb.sw_drv.gen_rand_full(transaction_n))
+
+    for i in range(transaction_n):
+        n = randint(0, 4)
+        await swtb.sw_drv.write_to_n_inputs(n)
+
+    await swtb.sw_drv.clear_sw_input(True)
+    dut.nxt_fifo_full_i <= 0
+
+    await ClockCycles(dut.clk_i, 10)
+
+    await ClockCycles(dut.clk_i, 100)
+
+    swtb.compare()
+
+if int(os.environ["TESTFACTORY"]):
+    tf = TestFactory(test_function=test_rand_single_input)
+    tf.add_option(name='transaction_w', optionlist=[*range(16)])
+    tf.add_option(name='with_nxt_fifo_rand', optionlist=[True, False])
+    tf.generate_tests()
+    tf2 = TestFactory(test_function=test_rand_multi_input)
+    tf2.add_option(name='transaction_w', optionlist=[*range(16)])
+    tf2.add_option(name='with_nxt_fifo_rand', optionlist=[True, False])
+    tf2.generate_tests()
+
+
+async def send255(swtb):
+    for i in range(10):
+        await send5way(swtb)
+        await send5way(swtb)
+        await send5way(swtb)
+        await send5way(swtb)
+        await send5way(swtb)
+    await send5way(swtb)
 
 
 async def send5way(swtb):
     await swtb.sw_drv.write_to_single_input(0, 1, 1)  # from Reasource to Resource
-    await swtb.sw_drv.write_to_single_input(1, 0, 1)  # sfromend left
-    await swtb.sw_drv.write_to_single_input(2, 2, 1)  # send right
+    await swtb.sw_drv.write_to_single_input(2, 0, 1)  # send left
+    await swtb.sw_drv.write_to_single_input(1, 2, 1)  # send right
     await swtb.sw_drv.write_to_single_input(3, 1, 2)  # send down
     await swtb.sw_drv.write_to_single_input(4, 1, 0)  # send up