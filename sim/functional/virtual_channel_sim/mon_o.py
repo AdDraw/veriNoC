45655f2d773519c1fb080ab852e0f9c80a1dfe47
AdDraw <drawc.adam.priv@gmail.com>
2021-05-09 17:29:45 +0200

    Mesh Wormhole Node alpha version (alpha -> not tested) - mesh wormhole node passes SYNTH (added mesh_wormhole_node.tcl) - Virtual Channel rework (wired rd_en instead of a registered rd_en) - first tb for virtual_channel (simple testcase + simple driver) - slight parameter circ_fifo rename (WIDTH -> W)

---
+++ sim/functional/virtual_channel_sim/mon_o.py
@@ -0,0 +1,45 @@
+import cocotb
+from cocotb_bus.monitors import BusMonitor
+from cocotb.triggers import RisingEdge, ReadOnly
+
+from logging import INFO, DEBUG
+
+
+class FifoOMon(BusMonitor):
+    _signals = ["data_o", "empty_o", "full_o", "underflow_o", "overflow_o"]
+
+    def __init__(self, entity, name, clock, log_lvl=INFO, callback=None):
+        self.entity = entity
+        self.name = name
+        self.clock = clock
+        BusMonitor.__init__(self, entity, name, clock, callback=callback)
+        if log_lvl == DEBUG:
+            self.log.setLevel(DEBUG)
+        else:
+            self.log.setLevel(INFO)
+
+    @cocotb.coroutine
+    async def _monitor_recv(self):
+        # Monitor Init
+        clkedg = RisingEdge(self.clock)
+        ro = ReadOnly()
+
+        while True:
+            # Capture the output data
+            await clkedg
+            await ro
+            bus_values = self.bus.capture()
+            data_o = bus_values["data_o"].value
+            empty_o = bus_values["empty_o"].value
+            full_o = bus_values["full_o"].value
+            underflow_o = bus_values["underflow_o"].value
+            overflow_o = bus_values["overflow_o"].value
+
+            cycle_results = {"data": data_o,
+                             "empty": empty_o,
+                             "full": full_o,
+                             "underflow": underflow_o,
+                             "overflow": overflow_o}
+
+            self.log.debug(f'T_o: {cycle_results}')
+            self._recv(cycle_results)
---
+++ sim/functional/virtual_channel_sim/test.py
@@ -0,0 +1,70 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.result import TestSuccess, TestError
+from cocotb.triggers import ClockCycles, RisingEdge
+from cocotb_bus.scoreboard import Scoreboard
+
+from driver import VCDriver
+# from mon_i import FifoIMon
+# from mon_o import FifoOMon
+
+import os
+
+from logging import INFO, DEBUG
+
+CLOCK_PERIOD_NS = int(os.environ["CLK_PERIOD"])
+
+class VCTB:
+    def __init__(self, dut, log_lvl=INFO):
+        self.dut = dut
+
+        self.config = {
+            "data_w": int(os.environ["DATA_W"]),
+            "vc_depth_w": int(os.environ["VC_DEPTH_W"]),
+            "id_w": int(os.environ["ID_W"]),
+        }
+
+        assert self.dut.DATA_W == self.config["data_w"], "Bad Value"
+        assert self.dut.VC_DEPTH_W == self.config["vc_depth_w"], "Bad Value"
+        assert self.dut.ID_W == self.config["id_w"], "Bad Value"
+
+        self.vc_drv = VCDriver(dut, "", dut.clk_i, self.config, log_lvl)
+        # self.vc_i_mon = VCIMon(dut, "", dut.clk_i, log_lvl, callback=self.mon_callback)
+        # self.vc_o_mon = VCOMon(dut, "", dut.clk_i, log_lvl)
+
+    def setup_dut(self, cycle_n):
+        cocotb.fork(self.vc_drv.reset())
+        cocotb.fork(Clock(self.dut.clk_i, CLOCK_PERIOD_NS).start(cycles=cycle_n))
+
+@cocotb.test()
+async def basic_test(dut, log_lvl=INFO, cycles=10000):
+    cocotb.log.info("----------------------------------------------------------------------------- Simulation Started!")
+    vctb = VCTB(dut, log_lvl)
+    vctb.setup_dut(cycle_n=cycles)
+    await ClockCycles(dut.clk_i, 10)
+    await vctb.vc_drv.send_packet(0)
+    await vctb.vc_drv.send_packet(0)
+    if dut.header_o.value != 0:
+        cocotb.log.info("1st packet")
+        while dut.data_o.value != 0b1100000000:
+            dut.chan_alloc_i <= 1
+            dut.chan_rdy_i <= 1
+            await RisingEdge(dut.clk_i)
+    dut.chan_alloc_i <= 0
+    dut.chan_rdy_i <= 0
+    await RisingEdge(dut.data_vld_o)
+
+    cocotb.log.info("DUN")
+    await RisingEdge(dut.clk_i)
+
+    if dut.header_o.value != 0:
+        cocotb.log.info("2nd packet")
+        while dut.data_o.value != 0b1100000000:
+            dut.chan_alloc_i <= 1
+            dut.chan_rdy_i <= 1
+            await RisingEdge(dut.clk_i)
+    dut.chan_alloc_i <= 0
+    dut.chan_rdy_i <= 0
+
+    await ClockCycles(dut.clk_i, 100)
+    raise TestSuccess("Sim finished sucessfully")
