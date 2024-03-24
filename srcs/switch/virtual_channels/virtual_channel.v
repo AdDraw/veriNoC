63b534732871248419383bee32a323a551f6f5a2
AdDraw <drawc.adam.priv@gmail.com>
2024-03-25 00:28:42 +0100

    virtual channel router update(req out instead of decimal codes)

--- srcs/switch/virtual_channels/virtual_channel.v
+++ srcs/switch/virtual_channels/virtual_channel.v
@@ -38,24 +38,25 @@ module virtual_channel #(
   parameter COL_CORD    = 1,
   parameter FLIT_ID_W   = 2,
   parameter ROW_CORD    = 1,
-  parameter OUT_N_W     = 3,
+  parameter OUT_M       = 5,
   parameter COL_ADDR_W  = 2,
-  parameter ROW_ADDR_W  = 2
+  parameter ROW_ADDR_W  = 2,
+  parameter HEADER_ID   = 0,
+  parameter TAIL_ID     = 0,
+  parameter FLIT_W      = 0
 ) (
   input                clk_i,
   input                rst_ni,
   // FIFO based input (data & wr_enable)
-  input  [`FLIT_W-1:0] data_i,
+  input  [ FLIT_W-1:0] data_i,
   input                wr_en_i,
   // Allocator info input
   input                chan_alloc_i,     // HEADER won the competition info
   input                chan_rdy_i,       // BUFFER on the other side is not full
   // To Route
-  output [`FLIT_W-1:0] data_o,
+  output [ FLIT_W-1:0] data_o,
   output               data_vld_o,
-  output [`FLIT_W-1:0] header_o,
-  output [OUT_N_W-1:0] route_res_o,
-  output               route_res_vld_o,
+  output [OUT_M-1:0]   req_o,
   // FIFO based output
   output               rdy_o             // backpressure signal
 );
@@ -65,35 +66,41 @@ module virtual_channel #(
   localparam IDLE = 3'b001;
   localparam WAITING = 3'b010;
   localparam ACTIVE = 3'b100;
-  reg  [FSM_SIZE-1:0] cur_fsm_state;
-  reg  [FSM_SIZE-1:0] nxt_fsm_state_v;
+  reg  [  FSM_SIZE-1:0] cur_fsm_state;
+  reg  [  FSM_SIZE-1:0] nxt_fsm_state;
 
   // Header
-  reg  [ `FLIT_W-1:0] nxt_header_v;
-  reg  [ `FLIT_W-1:0] cur_header;
+  reg  [ FLIT_ID_W-1:0] cur_hdr_id;
+  reg  [COL_ADDR_W-1:0] cur_hdr_col_addr;
+  reg  [ROW_ADDR_W-1:0] cur_hdr_row_addr;
+
+  reg  [ FLIT_ID_W-1:0] nxt_hdr_id;
+  reg  [COL_ADDR_W-1:0] nxt_hdr_col_addr;
+  reg  [ROW_ADDR_W-1:0] nxt_hdr_row_addr;
 
   // FIFO signals
-  wire [ `FLIT_W-1:0] data_w;
-  wire                empty_w;
-  wire                full_w;
-  wire                underflow_w;
-  reg                 rd_en_v;
-  reg                 data_vld;
-  reg                 data_vld_v;
-
-  // Router
-  wire [ OUT_N_W-1:0] route_res_w;
+  wire [    FLIT_W-1:0] data_w;
+  wire                  empty_w;
+  wire                  full_w;
+  wire                  underflow_w;
+  reg                   rd_en_nxt;
+  reg                   data_vld;
+  reg                   data_vld_nxt;
 
   // updater
   always @(posedge clk_i or negedge rst_ni) begin : SYNC_UPDATE
     if (!rst_ni) begin
-      cur_fsm_state <= IDLE;
-      cur_header    <= 0;
-      data_vld      <= 0;
+      cur_fsm_state    <= IDLE;
+      cur_hdr_id       <= {FLIT_ID_W{1'b0}};
+      cur_hdr_col_addr <= {COL_ADDR_W{1'b0}};
+      cur_hdr_row_addr <= {ROW_ADDR_W{1'b0}};
+      data_vld         <= 0;
     end else begin
-      cur_fsm_state <= nxt_fsm_state_v;
-      cur_header    <= nxt_header_v;
-      data_vld      <= data_vld_v;
+      cur_fsm_state    <= nxt_fsm_state;
+      cur_hdr_id       <= nxt_hdr_id;
+      cur_hdr_col_addr <= nxt_hdr_col_addr;
+      cur_hdr_row_addr <= nxt_hdr_row_addr;
+      data_vld         <= data_vld_nxt;
     end
   end
 
@@ -101,54 +108,61 @@ module virtual_channel #(
   always @(*) begin : FSM_COMBO
     case (cur_fsm_state)
       IDLE:
-      if (data_w[`FLIT_ID_RANGE] == `HEADER_ID) nxt_fsm_state_v <= WAITING;
-      else nxt_fsm_state_v <= IDLE;
+      if (data_w[FLIT_W-:FLIT_ID_W] == HEADER_ID) nxt_fsm_state <= WAITING;
+      else nxt_fsm_state <= IDLE;
       WAITING:
-      if (chan_alloc_i && chan_rdy_i) nxt_fsm_state_v <= ACTIVE;
-      else nxt_fsm_state_v <= WAITING;
+      if (chan_alloc_i && chan_rdy_i) nxt_fsm_state <= ACTIVE;
+      else nxt_fsm_state <= WAITING;
       ACTIVE:
-      if (data_w[`FLIT_ID_RANGE] == `TAIL_ID && chan_rdy_i) nxt_fsm_state_v <= IDLE;
-      else nxt_fsm_state_v <= ACTIVE;
-
-      default: nxt_fsm_state_v <= IDLE;
+      if (data_w[FLIT_W-:FLIT_ID_W] == TAIL_ID && chan_rdy_i) nxt_fsm_state <= IDLE;
+      else nxt_fsm_state <= ACTIVE;
+      default: nxt_fsm_state <= IDLE;
     endcase
   end
 
   // RD_EN & CURRENT_HEADER CONTROL
   always @(*) begin : LOGIC_COMBO
-    case (nxt_fsm_state_v)
+    case (nxt_fsm_state)
       IDLE: begin
-        rd_en_v      = ~empty_w & ~data_vld & !underflow_w;
-        nxt_header_v = cur_header;
+        rd_en_nxt          = ~empty_w & ~data_vld & !underflow_w;
+        nxt_hdr_id       = cur_hdr_id;
+        nxt_hdr_col_addr = cur_hdr_col_addr;
+        nxt_hdr_row_addr = cur_hdr_row_addr;
       end
       WAITING: begin
-        rd_en_v      = 1'b0;
-        nxt_header_v = data_w;
+        rd_en_nxt          = 1'b0;
+        nxt_hdr_id       = data_w[ROW_ADDR_W+COL_ADDR_W+:FLIT_ID_W];
+        nxt_hdr_col_addr = data_w[ROW_ADDR_W+:COL_ADDR_W];
+        nxt_hdr_row_addr = data_w[0+:ROW_ADDR_W];
       end
       ACTIVE: begin
-        rd_en_v      = ~empty_w & chan_rdy_i & !underflow_w;
-        nxt_header_v = cur_header;
+        rd_en_nxt          = ~empty_w & chan_rdy_i & !underflow_w;
+        nxt_hdr_id       = cur_hdr_id;
+        nxt_hdr_col_addr = cur_hdr_col_addr;
+        nxt_hdr_row_addr = cur_hdr_row_addr;
       end
       default: begin
-        rd_en_v      = 1'b0;
-        nxt_header_v = 0;
+        rd_en_nxt          = 1'b0;
+        nxt_hdr_id       = {FLIT_ID_W{1'b0}};
+        nxt_hdr_col_addr = {COL_ADDR_W{1'b0}};
+        nxt_hdr_row_addr = {ROW_ADDR_W{1'b0}};
       end
     endcase
   end
 
   always @(*) begin
     case (cur_fsm_state)
-      IDLE:    data_vld_v <= 1'b0;
-      WAITING: data_vld_v <= 1'b1;
-      ACTIVE:  if (!chan_rdy_i && data_vld) data_vld_v <= 1'b1;
- else data_vld_v <= rd_en_v;
-      default: data_vld_v <= 0;
+      IDLE:    data_vld_nxt <= 1'b0;
+      WAITING: data_vld_nxt <= 1'b1;
+      ACTIVE:  if (!chan_rdy_i && data_vld) data_vld_nxt <= 1'b1;
+               else data_vld_nxt <= rd_en_nxt;
+      default: data_vld_nxt <= 0;
     endcase
   end
 
   // Circular FIFO
   circ_fifo #(
-    .DATA_W      (`FLIT_W),
+    .DATA_W      (FLIT_W),
     .FIFO_DEPTH_W(VC_DEPTH_W)
   ) buffer (
     .clk_i      (clk_i),
@@ -169,16 +183,16 @@ module virtual_channel #(
     .ROW_CORD  (ROW_CORD),
     .COL_ADDR_W(COL_ADDR_W),
     .ROW_ADDR_W(ROW_ADDR_W),
-    .OUT_N_W   (OUT_N_W)
+    .OUT_M     (OUT_M)
   ) x_router (
-    .col_addr_i    (cur_header[`COL_ADDR_RANGE]),
-    .row_addr_i    (cur_header[`ROW_ADDR_RANGE]),
-    .out_chan_sel_o(route_res_o)
+    .col_addr_i(cur_hdr_col_addr),
+    .row_addr_i(cur_hdr_row_addr),
+    .oc_sel_o  (req_o)
   );
 
+  // Change routing to requests
   assign data_o          = (cur_fsm_state != IDLE) ? data_w : 0;
   assign rdy_o           = ~full_w;
-  assign header_o        = cur_header;
   assign route_res_vld_o = (cur_fsm_state == WAITING) ? 1'b1 : 1'b0;
   assign data_vld_o      = (chan_rdy_i) ? data_vld : 1'b0;
 
